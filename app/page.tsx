8c56ddfa1283893c8c84ffdca230ac70421693a2
yeaseula <kminea@naver.com>
2025-12-08 20:45:13 +0900

    react hook roule 규정

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 "use client"
 import Footer from "./components/footer/Footer";
 import MainPage from "./components/mainpage";
-import { useAuthStore } from "./lib/userfetch";
 
 export default function mainHome() {
-
-  const { session, profile } = useAuthStore()
 
   return(
     <div className="common-wrap">
